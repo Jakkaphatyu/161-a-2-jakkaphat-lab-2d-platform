1de5ab4914b4712d80167b88ea62d15b793b2be2
Jakkaphat.yu <jakkaphat.yu@bumail.net>
2025-10-14 11:00:42 +0700

    Add on Charecter Scripts inherrited

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    private int health;
+    public int Health { get => health; set => health = (value < 0) ? 0 : value; }
+
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    //methods
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Debug.Log($"{this.name} took damage {damage}. Current Health: {Health}");
+    }
+
+    public bool IsDead()
+    {
+        if (Health <= 0)
+        {
+            Destroy(this.gameObject);
+            Debug.Log($"{this.name} is dead! and destroyed!");
+            return true;
+        }
+        else return false;
+
+    }
+}
